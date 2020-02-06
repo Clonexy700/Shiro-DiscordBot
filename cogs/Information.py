c641f4ac73d3b24ff01aec929a075094ce6ad378
Clonexy700 <55646391+Clonexy700@users.noreply.github.com>
2020-02-06 18:37:24 +0700

    colour and urban

--- cogs/Information.py
+++ cogs/Information.py
@@ -8,6 +8,8 @@ import re
 
 from io import BytesIO
 from discord.ext import commands
+from utility import http
+
 
 class Info(commands.Cog):
     def __init__(self, client):
@@ -23,29 +25,56 @@ class Info(commands.Cog):
 
         await ctx.send(r[endpoint])
 
-    @commands.command()
-    @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
-    async def cat(self, ctx):
-        """ Posts a random cat """
-        await self.randomimageapi(ctx, 'https://api.alexflipnote.dev/cats', 'file')
+    @commands.command(aliases=['color'])
+    async def colour(self, ctx, colour: str):
+        if colour == "random":
+            colour = "%06x" % random.randint(0, 0xFFFFFF)
+
+        if colour[:1] == "#":
+            colour = colour[1:]
+
+        if not re.search(r'^(?:[0-9a-fA-F]{3}){1,2}$', colour):
+            return await ctx.send("You're only allowed to enter HEX (0-9 & A-F)")
+
+        try:
+            r = await http.get(f"https://api.alexflipnote.dev/colour/{colour}", res_method="json", no_cache=True)
+        except aiohttp.ClientConnectorError:
+            return await ctx.send("The API seems to be down...")
+        except aiohttp.ContentTypeError:
+            return await ctx.send("The API returned an error or didn't return JSON...")
+
+        embed = discord.Embed(colour=discord.Colour.dark_purple())
+        embed.set_thumbnail(url=r["image"])
+        embed.set_image(url=r["image_gradient"])
+
+        embed.add_field(name="HEX", value=r['hex'], inline=True)
+        embed.add_field(name="RGB", value=r['rgb'], inline=True)
+        embed.add_field(name="Int", value=r['int'], inline=True)
+        embed.add_field(name="Brightness", value=r['brightness'], inline=True)
+
+        await ctx.send(embed=embed, content=f"{ctx.invoked_with.title()} name: **{r['name']}**")
 
     @commands.command()
-    @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
-    async def dog(self, ctx):
-        """ Posts a random dog """
-        await self.randomimageapi(ctx, 'https://api.alexflipnote.dev/dogs', 'file')
+    @commands.cooldown(rate=1, per=2.0, type=commands.BucketType.user)
+    async def urban(self, ctx, *, search: str):
+        async with ctx.channel.typing():
+            url = await http.get(f'https://api.urbandictionary.com/v0/define?term={search}', res_method="json")
 
-    @commands.command(aliases=["bird"])
-    @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
-    async def birb(self, ctx):
-        """ Posts a random birb """
-        await self.randomimageapi(ctx, 'https://api.alexflipnote.dev/birb', 'file')
+            if url is None:
+                return await ctx.send("I think the API broke...")
 
-    @commands.command()
-    @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
-    async def duck(self, ctx):
-        """ Posts a random duck """
-        await self.randomimageapi(ctx, 'https://random-d.uk/api/v1/random', 'url')
+            if not len(url['list']):
+                return await ctx.send("Couldn't find your search in the dictionary...")
+
+            result = sorted(url['list'], reverse=True, key=lambda g: int(g["thumbs_up"]))[0]
+
+            definition = result['definition']
+            if len(definition) >= 1000:
+                definition = definition[:1000]
+                definition = definition.rsplit(' ', 1)[0]
+                definition += '...'
+
+            await ctx.send(f":bookmark:  Definitions for **{result['word']}**```fix\n{definition}```")
 
 def setup(client):
     client.add_cog(Info(client))
