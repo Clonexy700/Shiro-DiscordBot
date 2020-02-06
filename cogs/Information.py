a91aa485b1d0c31def9bb9e509d0b6a2864abe37
Clonexy700 <55646391+Clonexy700@users.noreply.github.com>
2020-02-06 18:26:22 +0700

    a

---
+++ cogs/Information.py
@@ -0,0 +1,51 @@
+import random
+import discord
+import urllib
+import secrets
+import asyncio
+import aiohttp
+import re
+
+from io import BytesIO
+from discord.ext import commands
+
+class Info(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    async def randomimageapi(self, ctx, url, endpoint):
+        try:
+            r = await http.get(url, res_method="json", no_cache=True)
+        except aiohttp.ClientConnectorError:
+            return await ctx.send("The API seems to be down...")
+        except aiohttp.ContentTypeError:
+            return await ctx.send("The API returned an error or didn't return JSON...")
+
+        await ctx.send(r[endpoint])
+
+    @commands.command()
+    @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
+    async def cat(self, ctx):
+        """ Posts a random cat """
+        await self.randomimageapi(ctx, 'https://api.alexflipnote.dev/cats', 'file')
+
+    @commands.command()
+    @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
+    async def dog(self, ctx):
+        """ Posts a random dog """
+        await self.randomimageapi(ctx, 'https://api.alexflipnote.dev/dogs', 'file')
+
+    @commands.command(aliases=["bird"])
+    @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
+    async def birb(self, ctx):
+        """ Posts a random birb """
+        await self.randomimageapi(ctx, 'https://api.alexflipnote.dev/birb', 'file')
+
+    @commands.command()
+    @commands.cooldown(rate=1, per=1.5, type=commands.BucketType.user)
+    async def duck(self, ctx):
+        """ Posts a random duck """
+        await self.randomimageapi(ctx, 'https://random-d.uk/api/v1/random', 'url')
+
+def setup(client):
+    client.add_cog(Info(client))
